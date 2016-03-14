b875040c9a6a1e0a9da197d76072987fc96d9efa
Heather Conley <hconley6695@gmail.com>
2016-03-14 17:23:45 -0400

    finished functions and html

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,22 @@
 // Javascript Entry Point
+import $ from 'jquery';
+
+import form_data from './form_data';
+
+import { makeInputs, commentSection, selectBox } from './templates';
+
+
+var $app = $('.app');
+
+form_data.forEach(function(datum) {
+	var template;
+	if (datum.type === "text" || datum.type === "email" || datum.type === "tel") {
+		template = makeInputs;
+	} else if (datum.type === "textarea") {
+		template = commentSection;
+	} else if (datum.type === "select") {
+		template = selectBox;
+	}
+	var html = template(datum);
+	$app.append(html);
+});
--- src/js/templates.js
+++ src/js/templates.js
@@ -1,3 +1,40 @@
 
+export function makeInputs(input) {
+	return `
+	<label for="${input.id}">      </label>
+ 		<input type="text" 	id="${input.id}" value="${input.label}">
+ 		<br>
+ 		`
+};
+
+export function commentSection(comment) {
+	return `
+		<label for="${comment.id}">    </label>
+		<input type="textarea" id="${comment.id}" value="${comment.label}">
+ 		<br>
+ 		`
+};
+
+export function selectBox(language){
+	var langOpt = language.options;
+	var langHtml = langOpt.map (function(lang) {
+		return `<option>${lang }</option>`
+	});	
+
+	langHtml = langHtml.join('');
+
+	return `
+	 	<label for="${language.id}">     </label>
+	 	<select id="${language.id}"> 
+ 			<option value="Select Language">Select Language</Option>
+ 			<option value="EN">English</Option>
+ 			<option value="FR">French</Option>
+ 			<option value="SP">Spanish</Option>
+ 			<option value="CH">Chinese</Option>
+ 			<option value="JP">Japanese</Option>
+ 		</select>
+ 		<br>
+ 	`
+};
 
 
